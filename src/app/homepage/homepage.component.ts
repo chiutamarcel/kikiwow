55707587b97cf0227e1dca05c811242ec07d369a
chiutamarcel <chiuta.marcel@gmail.com>
2024-04-12 14:48:22 +0200

    - homepage: added news

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,12 +1,31 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+
+interface NewsItem{
+  id:number;
+  description:string;
+};
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
-export class HomepageComponent {
 
+export class HomepageComponent {
+  news: NewsItem[];
+
+  constructor() {
+    this.news = [];
+
+    let phText = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. \
+                  In odit qui sunt autem iure non quod suscipit eius, a cumque laborum ex, \
+                  atque dolore neque. Eum recusandae officiis possimus dignissimo.";
+
+    for (let i = 0; i < 10; i++) {
+      this.news.push({ id:i, description:phText });
+    }
+  }
 }
