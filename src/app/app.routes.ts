81b426c933e256c52261f3966a1ca577b6ff95de
chiutamarcel <chiuta.marcel@gmail.com>
2024-04-12 16:06:01 +0200

    - general: added login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {path: '', component: HomepageComponent},
@@ -7,6 +8,6 @@ export const routes: Routes = [
     {path: 'rules', component: HomepageComponent},
     {path: 'realms', component: HomepageComponent},
     {path: 'database', component: HomepageComponent},
-    {path: 'login', component: HomepageComponent},
+    {path: 'login', component: LoginComponent},
     {path: 'register', component: HomepageComponent}
 ];
